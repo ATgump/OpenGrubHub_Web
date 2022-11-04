e4110fb00b6efab4eb0e0136c1350be75cc17d7a
ZYzhang24 <113130502+ZYzhang24@users.noreply.github.com>
2022-11-04 15:07:29 -0400

    Add files via upload
    To install django-admin-interface. https://github.com/fabiocaccamo/django-admin-interface

--- OpenGrubHub_Web/settings.py
+++ OpenGrubHub_Web/settings.py
@@ -35,6 +35,8 @@ LOGIN_URL = "/login"
 # Application definition
 # FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -149,9 +151,20 @@ STATICFILES_DIRS = [
     BASE_DIR / "EntryPage/static",
     BASE_DIR / "Profiles/static",
     BASE_DIR / "Community/static",
+    str(BASE_DIR.joinpath('static')),
+]
+
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# only if django version >= 3.0
+X_FRAME_OPTIONS = "SAMEORIGIN"
+SILENCED_SYSTEM_CHECKS = ["security.W019"]
